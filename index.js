0e90a53a3eb89b15fdd2fd5ec15ea233cc4826c0
FernandoDLT <ferchodlt1971@gmail.com>
2025-01-25 17:13:48 -0600

    Add coding challenge #2

--- index.js
+++ index.js
@@ -16,35 +16,3 @@ if (true) {
 }
 // logs 10 (the outer-scoped 'x', completely untouched by the block condition)
 console.log(x)
-
-// #2
-// The 'user' constant is assigned an object reference
-const user = {
-   name: "Fernando"
-};
-
-// Properties inside the obj can be mutated/reassigned
-user.name = "Ronald"
-
-// Will log 'Ronald' (the reassigned property value)
-console.log(user.name)
-
-// Question 3
-// 'var a' is hoisted to the top scope, 
-// but initialized as 'undefined'
-console.log(b); // logs undefined
-
-// 'a' is assigned the value 10 here
-var b = 10;
-
-// var a;         // Hoisted to the top with value 'undefined'
-// console.log(a); // Logs 'undefined'
-// a = 10;        // Assignment stays in place
-
-// Question #4
-// 'c' is hoisted, but resides in the Temporal Dead Zone (TDZ)
-// until initialized
-console.log(c); // ReferencError: Cannot access 'c' before initialization
-
-// 'c' is initialized here
-let c = 10;
